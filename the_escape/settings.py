43190505df7aa265708d04e3889fb77bb6814af8
elhaj101 <alielhajj@outlook.de>
2025-04-05 21:28:10 +0200

    create templates and settings

--- the_escape/settings.py
+++ the_escape/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-368qmk&^ncfj%)#n^l0cy+h%olbm64!^$66h$x9!w@xsi$th5)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -58,6 +58,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
